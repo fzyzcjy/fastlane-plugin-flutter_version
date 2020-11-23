bea18228b255af75df20cdf5a56e9a185c16d9d2
fzyzcjy <turtlegood@163.com>
2020-11-23 08:20:40 +0800

    more

--- lib/fastlane/plugin/increment_flutter_version/actions/check_git_commit.rb
+++ lib/fastlane/plugin/increment_flutter_version/actions/check_git_commit.rb
@@ -14,17 +14,20 @@ module Fastlane
         git_last_commit_log = Actions.sh('git --no-pager log -n1 --pretty=%B')
         UI.message("git_last_commit_log=#{git_last_commit_log}")
         unless expect_regex.match?(git_last_commit_log)
-          UI.user_error!("Unexpected git last commit log. Should be `#{expect_regex}` but found `#{git_last_commit_log}`.")
+          UI.message("Unexpected git last commit log. Should be `#{expect_regex}` but found `#{git_last_commit_log}`.")
+          return false
         end
 
         # NOTE 2. check the working dir is not dirty
         # copy from: commit_version_bump
         git_dirty_files = Actions.sh('git diff --name-only HEAD').split("\n") + Actions.sh('git ls-files --other --exclude-standard').split("\n")
         if git_dirty_files.length > 0
-          UI.user_error!("Found unexpected uncommitted changes in the working directory. git_dirty_files=#{git_dirty_files}")
+          UI.message("Found unexpected uncommitted changes in the working directory. git_dirty_files=#{git_dirty_files}")
+          return false
         end
 
         UI.success("pass check_git_commit checkers.")
+        return true
       end
 
       def self.authors
